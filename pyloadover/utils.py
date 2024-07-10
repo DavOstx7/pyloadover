0034f23e197d74dc0e5f87a3d5083b481258888d
dav ost <private>
2024-07-10 17:32:52 +0300

    Add get_namespace util + tests

--- pyloadover/utils.py
+++ pyloadover/utils.py
@@ -1,4 +1,5 @@
 import typeguard
+from typing import Callable
 
 
 def is_instance(value, annotation) -> bool:
@@ -8,3 +9,7 @@ def is_instance(value, annotation) -> bool:
         return False
     else:
         return True
+
+
+def get_namespace(function: Callable) -> str:
+    return f"{function.__module__}.{function.__name__}"
