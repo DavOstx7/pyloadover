9bad9e0b988ea810e3a2c0b4ca032da8b85ca6fa
dav ost <private>
2024-07-10 20:23:47 +0300

    Add dynamic pyloadover

--- pyloadover/__init__.py
+++ pyloadover/__init__.py
@@ -1,1 +1,18 @@
+from typing import Callable
 from .pyloadover import pyloadover, loadover
+
+
+class _DynamicPyloadover:
+    def __init__(self, name):
+        self.name = name
+
+    def __getattr__(self, item):
+        self.name = f"{self.name}.{item}"
+        return self
+
+    def __call__(self, f: Callable):
+        return pyloadover(self.name)(f)
+
+
+def __getattr__(group: str):
+    return _DynamicPyloadover(group)
