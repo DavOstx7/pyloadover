058aaf6851bba0fe7af9e48842e74f4eb2c3f6b5
dav ost <private>
2024-07-20 17:06:36 +0300

    Add support for wrapped/bound functions/methods

--- pyloadover/functions/context.py
+++ pyloadover/functions/context.py
@@ -1,11 +1,15 @@
 import inspect
-from typing import Callable, Any
+from typing import Callable, Any, Optional
+from pyloadover.utils import get_underlying_callable
 
 
 class FunctionContext:
     def __init__(self, f: Callable[[...], Any]):
+        underlying_callable = get_underlying_callable(f)
+
         self._callable = f
-        self._signature = inspect.signature(f)
+        self._underlying_callable = underlying_callable
+        self._signature = inspect.signature(underlying_callable) if underlying_callable else inspect.signature(f)
 
     @property
     def callable(self) -> Callable[[...], Any]:
@@ -22,6 +26,10 @@ class FunctionContext:
     @property
     def module(self) -> str:
         return self._callable.__module__
+
+    @property
+    def underlying_callable(self) -> Optional[Callable[[...], Any]]:
+        return self._underlying_callable
 
     @property
     def signature(self) -> inspect.Signature:
--- pyloadover/utils.py
+++ pyloadover/utils.py
@@ -1,4 +1,6 @@
+import inspect
 import typeguard
+from typing import Optional, Callable, Any
 
 
 def is_instance(value, annotation) -> bool:
@@ -8,3 +10,30 @@ def is_instance(value, annotation) -> bool:
         return False
     else:
         return True
+
+
+def is_wrapped(obj: object) -> bool:
+    return hasattr(obj, '__wrapped__')
+
+
+def is_bound(obj: object) -> bool:
+    return hasattr(obj, '__func__')
+
+
+def unbind(obj: object) -> object:
+    return getattr(obj, '__func__')
+
+
+def get_underlying_callable(f: Callable[[...], Any]) -> Optional[Callable[[...], Any]]:
+    obj = f
+    while is_wrapped(obj) or is_bound(obj):
+        # Unwrapping before unbinding would always result in the shortest chain (even though it might skip some objects)
+        if is_wrapped(obj):
+            obj = inspect.unwrap(obj)
+        if is_bound(obj):
+            obj = unbind(obj)
+
+    if obj == f:
+        return None
+
+    return obj
