4f76eb4863de213e4ccc989363ed5f3137b5c209
dav ost <private>
2024-07-23 21:17:14 +0300

    More realistic test case for context signature attribute (no mocks)

--- tests/functions/test_context.py
+++ tests/functions/test_context.py
@@ -4,7 +4,7 @@ from pyloadover.functions.context import FunctionContext
 
 
 @patch('pyloadover.functions.context.get_underlying_callable', autospec=True)
-def test_underlying_callable_attribute(mock_get_underlying_callable: MagicMock, mock_callable):
+def test_underlying_callable_attr(mock_get_underlying_callable: MagicMock, mock_callable):
     context = FunctionContext(mock_callable)
 
     mock_get_underlying_callable.assert_called_once_with(mock_callable)
@@ -12,28 +12,18 @@ def test_underlying_callable_attribute(mock_get_underlying_callable: MagicMock, 
 
 
 @patch('inspect.signature', autospec=True)
-@patch('pyloadover.functions.context.get_underlying_callable', autospec=True)
-def test_signature_attribute_assigned_to_underlying_callable_signature(mock_get_underlying_callable: MagicMock,
-                                                                       mock_signature: MagicMock,
-                                                                       mock_callable, mock_underlying_callable):
-    mock_get_underlying_callable.return_value = mock_underlying_callable
-
+def test_signature_attr(mock_signature: MagicMock, mock_callable, mock_underlying_callable):
     context = FunctionContext(mock_callable)
 
-    mock_get_underlying_callable.assert_called_once_with(mock_callable)
-    mock_signature.assert_called_once_with(mock_underlying_callable)
+    mock_signature.assert_called_once_with(mock_callable)
     assert context._signature == mock_signature.return_value
 
 
 @patch('inspect.signature', autospec=True)
-@patch('pyloadover.functions.context.get_underlying_callable', autospec=True)
-def test_signature_attribute_assigned_to_callable_signature(mock_get_underlying_callable: MagicMock,
-                                                            mock_signature: MagicMock,
-                                                            mock_callable):
-    mock_get_underlying_callable.return_value = None
+def test_signature_attr_underlying_callable_exists(mock_signature: MagicMock, mock_callable, mock_underlying_callable):
+    mock_callable.__wrapped__ = mock_underlying_callable
 
     context = FunctionContext(mock_callable)
 
-    mock_get_underlying_callable.assert_called_once_with(mock_callable)
-    mock_signature.assert_called_once_with(mock_callable)
+    mock_signature.assert_called_once_with(mock_underlying_callable)
     assert context._signature == mock_signature.return_value
